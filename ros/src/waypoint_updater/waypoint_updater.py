38f54e1b1332a56c9e4e1be7e98419293760bf3b
charlielito <candres.alv@gmail.com>
2019-05-05 18:36:58 -0500

    fix

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -50,8 +50,9 @@ class WaypointUpdater(object):
         rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             if self.pose and self.base_waypoints:
-                closest_waypoint_idx = self.get_closest_waypoint_idx()
-                self.publish_waypoints(closest_waypoint_idx)
+                if self.waypoint_tree is not None:
+                    closest_waypoint_idx = self.get_closest_waypoint_idx()
+                    self.publish_waypoints(closest_waypoint_idx)
             rate.sleep()
 
     def get_closest_waypoint_idx(self):
